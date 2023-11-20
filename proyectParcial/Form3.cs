05ce7abb8b82c6b19102bd39c8203cb0e897cf47
enmanuelenrique654@gmail.com <enmanuelenrique654@gmail.com>
2023-11-20 08:31:26 -0600

    validamos todos los botones

--- proyectParcial/Form3.cs
+++ proyectParcial/Form3.cs
@@ -16,5 +16,59 @@ namespace proyectParcial
         {
             InitializeComponent();
         }
+            private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+            {
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                {
+                    // Si la tecla presionada no es un número, limpiar el TextBox
+                    textBox1.Clear();
+
+                    // Restablecer los elementos necesarios
+                    // ...
+
+                    // Indicar que se ha manejado el evento KeyPress
+                    e.Handled = true;
+                }
+            }
+            private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+            {
+                if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar))
+                {
+                    // Si la tecla presionada no es una letra, limpiar el TextBox
+                    textBox2.Clear();
+
+                    // Restablecer los elementos necesarios
+                    // ...
+
+                    // Indicar que se ha manejado el evento KeyPress
+                    e.Handled = true;
+                }
+            }
+            private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+            {
+                if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar))
+                {
+                    // Si la tecla presionada no es una letra, limpiar el TextBox
+                    textBox3.Clear();
+
+                    // Restablecer los elementos necesarios
+                    // ...
+
+                    // Indicar que se ha manejado el evento KeyPress
+                    e.Handled = true;
+                }
+            }
+            private void button1_Click(object sender, EventArgs e)
+            {
+                // Crear una instancia del formulario Form4
+                Form4 form4 = new Form4();
+
+                // Mostrar el formulario Form4
+                form4.Show();
+
+                // Opcionalmente, puedes ocultar el formulario actual si lo deseas
+                this.Hide();
+            }
+        }
     }
-}
+
